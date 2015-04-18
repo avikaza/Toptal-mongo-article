450a3ff03b27d330cbabcb01ee76beb045e28b5e
vkambati-github <vkambati@gmail.com>
2015-04-18 13:45:33 +0530

    Added sorting functionality

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,4 @@
 'use strict';
 
 // Declare app level module which depends on filters, and services
-var app = angular.module('myApp', [ 'ngGrid', 'ui.bootstrap']);
+var app = angular.module('myApp', [ 'ngGrid', 'ui.bootstrap', 'myApp.directives']);
--- public/js/directives.js
+++ public/js/directives.js
@@ -3,8 +3,27 @@
 /* Directives */
 
 angular.module('myApp.directives', []).
-  directive('appVersion', function (version) {
-    return function(scope, elm, attrs) {
-      elm.text(version);
-    };
-  });
+    directive('appVersion', function (version) {
+        return function(scope, elm, attrs) {
+            elm.text(version);
+        };
+    })
+    .directive('draggable', function(){
+        return{
+            restrict:"A",
+            link: function(scope, element, attributes){
+                console.log(element);
+                element.find(".panel-group").sortable({
+                    start: function( event, ui ) {
+                        scope.startIndex = ui.item.index();
+                    },
+                    stop: function( event, ui ) {
+                        scope.stopIndex = ui.item.index();
+                        scope.groups.move(scope.startIndex,  scope.stopIndex);
+                        scope.$apply();
+                    }
+                });
+                element.find(".panel-group" ).disableSelection();
+            }
+        }
+    });
